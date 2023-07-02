1088d0b63eeee2e70bf83eaa733300b9c008b5d7
Mudit <muditert34@gmail.com>
2023-07-02 15:04:33 +0530

    login and register route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
 import RegisterPage from "./pages/RegisterPage";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://127.0.0.1:4000";
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
--- server/index.js
+++ server/index.js
@@ -1,9 +1,73 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const User = require('./models/User.js')
+
 const app = express();
 
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+}));
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGO_URL)
 
 app.get('/', (req, res) => {
     res.json('test ok');
 })
 
-app.listen(4000);
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt)
+        })
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({
+                email: userDoc.email,
+                id: userDoc._id
+            }, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: 'none'
+                }).json(userDoc);
+            });
+        } else {
+            res.status(422).json('password incorrect');
+        }
+    } else {
+        res.json('not found');
+    }
+});
+
+app.listen(4000, () => {
+    console.log('listening on port 4000');
+});
