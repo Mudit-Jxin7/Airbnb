017ebfa0dfbd74371cf2476af46a6ccdb0399b62
Mudit <muditert34@gmail.com>
2023-07-03 16:12:39 +0530

    Account page and logout

---
+++ client/src/pages/Account.jsx
@@ -0,0 +1,68 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../userContext";
+import { Navigate, Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+const Account = () => {
+  const { ready, user, setUser } = useContext(UserContext);
+  const [redirect, setRedirect] = useState(null);
+
+  async function logout() {
+    await axios.post("/logout");
+    setRedirect("/");
+    setUser(null);
+  }
+
+  if (!ready) {
+    return "Loading...";
+  }
+
+  if (ready && !user && !redirect) {
+    return <Navigate to={"/login"} />;
+  }
+
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  function linkClasses(type = null) {
+    let classes = "inline-flex gap-1 py-2 px-6 rounded-full";
+    if (type === subpage) {
+      classes += " bg-primary text-white";
+    } else {
+      classes += " bg-gray-200";
+    }
+    return classes;
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+        <Link className={linkClasses("profile")} to={"/account"}>
+          My Profile
+        </Link>
+        <Link className={linkClasses("bookings")} to={"/account/bookings"}>
+          My Booking
+        </Link>
+        <Link className={linkClasses("places")} to={"/account/places"}>
+          My accommodations
+        </Link>
+      </nav>
+      {subpage === "profile" && (
+        <div className="text-center max-w-xl mx-auto">
+          Logged in as {user.name} ({user.email})
+          <button onClick={logout} className="primary max-w-sm mt-2">
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Account;
