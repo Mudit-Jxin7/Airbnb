de5efeef435271a541dc548c95b96aa4ff54ccb7
Mudit <muditert34@gmail.com>
2023-07-11 13:02:43 +0530

    listing user places

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import AccountNav from "../AccountNav";
+
 export default function PlacesPage() {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    axios.get("/places").then(({ data }) => {
+      setPlaces(data);
+    });
+  }, []);
+
   return (
     <div>
       <AccountNav />
@@ -23,6 +35,26 @@ export default function PlacesPage() {
           </svg>
           Add new place
         </Link>
+      </div>
+      <div className="mt-4">
+        {places.length > 0 &&
+          places.map((place) => (
+            <Link
+              to={"/account/places/" + place._id}
+              className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl"
+              key={place._id}
+            >
+              <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
+                {place.photos.length > 0 && (
+                  <img src={place.photos[0]} alt="" />
+                )}
+              </div>
+              <div className="grow-0 shrink">
+                <h2 className="text-xl">{place.title}</h2>
+                <p className="text-sm mt-2">{place.description}</p>
+              </div>
+            </Link>
+          ))}
       </div>
     </div>
   );
