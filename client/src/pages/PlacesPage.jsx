e39c59e1f9fa35e53b4ea8b8814980b9a1d0f0ae
Mudit <muditert34@gmail.com>
2023-07-20 22:19:57 +0530

    photo cover uploaded

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -46,7 +46,11 @@ export default function PlacesPage() {
             >
               <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
                 {place.photos.length > 0 && (
-                  <img src={place.photos[0]} alt="" />
+                  <img
+                    className="object-cover"
+                    src={"http://localhost:4000/uploads/" + place.photos[0]}
+                    alt=""
+                  />
                 )}
               </div>
               <div className="grow-0 shrink">
